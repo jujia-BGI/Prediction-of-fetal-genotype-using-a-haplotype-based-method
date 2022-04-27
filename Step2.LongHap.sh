fe1f22f04e5de0302166011b14e5475730015188
JuJia-bioinformatics <81357415+jujia-BGI@users.noreply.github.com>
2022-04-27 10:48:22 +0800

    Add files via upload

---
+++ Step2.LongHap.sh
@@ -0,0 +1,3 @@
+###LongHap is writen for individual genome phasing based on the alignment and variantion results generated from stLFR and NGS technology. Latest version was v1.3 upload in 10/17/2018.
+###https://github.com/stLFR/stLFR_LongHap/blob/master/Example/generate_shell.sh
+perl Main.pl input.bam input.vcf 10000000 300000 1 1 shDir outDir_tmp outDir_final
