f9473a1a745a9dce89ef793079bedc91ea0dd5ff
JuJia-bioinformatics <81357415+jujia-BGI@users.noreply.github.com>
2022-04-27 11:24:05 +0800

    Update Step3.haplotype_based_method.sh

--- Step3.haplotype_based_method.sh
+++ Step3.haplotype_based_method.sh
@@ -5,7 +5,7 @@ P_stLFR="TDP1807015714_P_stLFR" #paternal stLFR name in family merged vcf
 ff=0.15 #fetal fraction
 
 for i in `seq 1 22` X ;do
-        #input: <fetal fraction> <family merge vcf> <maternal haplotypes constructed by stLFR> <paternal haplotypes constructed by stLFR> <cfDNA name in vcf:TDP1807015083_cfDNA> <maternal name in vcf:TDP1807015084_M_stLFR> <paternal name in vcf:TDP1807015714_P_stLFR> 
+        #input: <fetal fraction> <family merged vcf> <maternal haplotypes constructed by stLFR> <paternal haplotypes constructed by stLFR> <cfDNA name in vcf:TDP1807015083_cfDNA> <maternal name in vcf:TDP1807015084_M_stLFR> <paternal name in vcf:TDP1807015714_P_stLFR> 
         perl Predict_haplotype_genotype.pl  $ff example/chr${i}.filter.vcf example/${M_stLFR}_chr${i}.vcf.gz  example/${P_stLFR}_chr${i}.vcf.gz $cfDNA  $M_stLFR  $P_stLFR > chr${i}.longhap.step1
         #input: <result of last step> <length in closest informative variant algorithm> <fetal fraction> < cfDNA name in vcf>
         perl Haplotype_adjustment_flank_length_SPRT_C5_haplotype_same_100kb.pl chr${i}.longhap.step1 100000 $ff $cfDNA > chr${i}.longhap.step2
